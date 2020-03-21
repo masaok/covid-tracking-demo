9726e4857c3c0eca8152377630a3615807911e34
Masao Kitamura <masaok@gmail.com>
2020-03-21 14:35:12 -0700

    SimpleLineChart works

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SimpleLineChart from './examples/SimpleLineChart'
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <SimpleLineChart />
       </header>
     </div>
   );
